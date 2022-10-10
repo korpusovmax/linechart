8d403da37d591c90c5416d1030e6f19229a9cb22
korpusovmaxim858 <korpusovmax@gmail.com>
2022-10-10 08:46:22 +0300

    Update examples

--- tests/simple_chart.js
+++ tests/simple_chart.js
@@ -2,5 +2,4 @@ import { LineChart } from './../src/linechart.js'
 
 let array = [8, 3, 7, 6, 9, 2];
 let linechart_1 = new LineChart("chart1", 400, 200, array);
-linechart_1.params['stroke-linecap'] = 'round';
 linechart_1.update();
