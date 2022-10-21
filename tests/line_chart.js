0b1b0b4f93f9eeae289860843518e2ddc582fac1
korpusovmaxim858 <korpusovmax@gmail.com>
2022-10-21 11:28:10 +0300

    Update examples

--- tests/line_chart.js
+++
@@ -1,9 +0,0 @@
-import { LineChart } from './../src/linechart.js'
-
-let array = [8, 3, 7, 6, 9, 2];
-let linechart_1 = new LineChart("mychart", 400, 200, array);
-linechart_1.params['segment'] = 'line';
-linechart_1.params['stroke'] = '#E82D2D';
-linechart_1.params['color'] = '#F24747';
-linechart_1.params['stroke-linecap'] = 'square';
-linechart_1.update();
